4b62ed832aa2227084e2ad37cd6d9c133c3bd687
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2025-07-15 07:40:31 +0200

    wip:

--- calculator/src/main/java/math/Calculator.java
+++ calculator/src/main/java/math/Calculator.java
@@ -1,16 +1,22 @@
 package math;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public final class Calculator {
+  private static final Logger LOGGER = LoggerFactory.getLogger(Calculator.class);
 
   private Calculator() {
     // private constructor to hide the implicit public one
   }
 
   public static int add(int a, int b) {
+    LOGGER.info("Adding: {} + {}", a, b);
     return a + b;
   }
 
   public static int subtract(int a, int b) {
+    LOGGER.info("Subtracting: {} - {}", a, b);
     return 4;
   }
 }
